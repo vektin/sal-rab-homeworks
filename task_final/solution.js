df8d9b6256c31a1c3b27839f8170ee69645991b2
Vektin Eugene <vektin@list.ru>
2023-02-05 22:46:48 +0300

    task_final done!

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,27 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
+    
     let data = {goods: [], order: {}};
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push(
+            {
+                title: goods[i].title,
+                count: goods[i].count
+            }
+        );
     }
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat}`;
 
-    data.client = 'Иван';
+    data.order.sum = sum;
 
-    let jsonData = JSON.stringify(data);
+    data.client = `${name} ${phone}`;
+
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 }
